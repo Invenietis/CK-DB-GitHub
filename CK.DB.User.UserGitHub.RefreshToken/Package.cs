1403c31de979cced00536961047b2b8997b00015
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-12-10 10:33:16 +0100

    Added CK.DB.User.UserGitHub.RefreshToken project.

---
+++ CK.DB.User.UserGitHub.RefreshToken/Package.cs
@@ -0,0 +1,17 @@
+using CK.Core;
+
+namespace CK.DB.User.UserGitHub.RefreshToken
+{
+    /// <summary>
+    /// Package that adds RefreshToken support to GitHub authentication.
+    /// </summary>
+    [SqlPackage( ResourcePath = "Res" )]
+    [Versions("1.0.0")]
+    [SqlObjectItem( "transform:CK.sUserGitHubUCL" )]
+    public abstract class Package : SqlPackage
+    {
+        void StObjConstruct( UserGitHub.UserGitHubTable table )
+        {
+        }
+    }
+}
