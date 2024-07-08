d5d78e37dea73dc7897c5a9b9144ad614966a861
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-07-08 11:05:37 +0200

    Use solution tests from CCB.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -51,7 +51,7 @@ namespace CodeCake
                .Does( () =>
                {
                     
-                  globalInfo.GetDotnetSolution().Test();
+                  globalInfo.GetDotnetSolution().SolutionTest();
                } );
 
             Task( "Create-NuGet-Packages" )
